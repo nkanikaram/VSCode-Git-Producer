e082e1a2c0deb07931469a72dbafb696cb7da751
Naveen Kanikaram <Naveen.Kanikaram@solera.com>
2018-09-25 14:14:32 -0500

    asdasdwqeqwe

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SimpleProducer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Added this to Master Branch");
             Console.ReadKey(true);//Adding Some Commentadsdsdfsdf adding Staging Change More changes  lot changes master changes
         }
     }
