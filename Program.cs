c8d1781db5b46dfc923aa563f1c9ca2881428035
Naveen Kanikaram <Naveen.Kanikaram@solera.com>
2018-09-25 00:07:26 -0500

    asdsad

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace SimpleProducer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey(true);//Adding Some Commentadsdsdfsdf adding Staging Change More changes  lot changes
+            Console.ReadKey(true);//Adding Some Commentadsdsdfsdf adding Staging Change More changes  lot changes master changes
         }
     }
 }
