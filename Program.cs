bcb83982f58e4075094102aa77ecf8f945fa9c79
Naveen Kanikaram <Naveen.Kanikaram@solera.com>
2018-09-27 16:43:46 -0500

    adasda

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace SimpleProducer
     {
         static void Main(string[] args)
         {
+            //Deleted 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Added this to Master Branch");
             Console.WriteLine("Added this to Master Branch2");
@@ -13,12 +14,6 @@ namespace SimpleProducer
             Console.WriteLine("Hello World");//asdasdasd - wewqeqweqweqwe -master changes
             Console.WriteLine("Propogate from Staging");
             Console.WriteLine("One more Change from Staging");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-
             Console.ReadKey(true);//Adding Some Commentadsdsdfsdf adding Staging Change More changes  lot changes master changes
         }
     }
