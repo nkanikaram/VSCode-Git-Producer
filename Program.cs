f194d4d8fd5d213f24b1a839f11ac2a4bcb1e316
Naveen Kanikaram <Naveen.Kanikaram@solera.com>
2018-09-26 22:43:36 -0500

    Testing commit from bash

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace SimpleProducer
             Console.WriteLine("Added this to Staging Branch");
             Console.WriteLine("Hello World");//asdasdasd - wewqeqweqweqwe -master changes
             Console.WriteLine("Propogate from Staging");
+            Console.WriteLine("One more Change from Staging");
 
             Console.ReadKey(true);//Adding Some Commentadsdsdfsdf adding Staging Change More changes  lot changes master changes
         }
